df95ecff2775e0138f403183e1211cf660e13b72
Matteo Quaratino <matteo.quaratino@tum.de>
2021-12-06 15:46:37 +0100

    calculate_target pos in python

--- catkin_ws/src/arm_control/scripts/node.py
+++ catkin_ws/src/arm_control/scripts/node.py
@@ -3,15 +3,38 @@ import rospy
 from geometry_msgs.msg import Point
 from std_msgs.msg import String
 
-pub = rospy.Publisher('rbt_topic', Point, queue_size=1000)
+pub = rospy.Publisher('target_pos', Point, queue_size=1000)
+sent = False
 
+
+def calculate_target_pos(cube_pos):
+    max_vel = [0.1,0.1,0.1,0.1]
+    max_accel = [0.02,0.02,0.02,0.02]
+    max_jerk = [0.01,0.01,0.01,0.01]
+
+    intermediate_point = cube_pos
+    intermediate_point.x += 0.1
+    intermediate_point.y += 0.1
+
+    return intermediate_point
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "X: %f", data.x)
-    rospy.loginfo(rospy.get_caller_id() + "Y: %f", data.y)
-    rospy.loginfo(rospy.get_caller_id() + "Z: %f", data.z)
-    print("\n")
-    pub.publish(data)
+    
+    # rospy.loginfo(rospy.get_caller_id() + "X: %f", data.x)
+    # rospy.loginfo(rospy.get_caller_id() + "Y: %f", data.y)
+    # rospy.loginfo(rospy.get_caller_id() + "Z: %f", data.z)
+    # print("\n")
+    global sent
+    if data.y > -0.5 and data.y < 0.5 and data.z < 0.6 and data.z > 0.1:
+        if sent == False:
+            print("test\n")
+            pub.publish(calculate_target_pos(data))
+
+            rospy.sleep(1)
+
+            pub.publish(data)
+
+            sent = True
     
     
 def listener():
@@ -23,10 +46,14 @@ def listener():
     # run simultaneously.
     rospy.init_node('rbt_node', anonymous=True)
 
-    rospy.Subscriber("target_pos", Point, callback)
+    rospy.Subscriber("cube_pos", Point, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
+
 if __name__ == '__main__':
+    sent = False
     listener()
+    
+
